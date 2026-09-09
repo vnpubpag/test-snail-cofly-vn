b24b85987fa14a5e2be06d5c9abcf4e14a3f7403
sonnt <sonnt148@gmail.com>
2026-09-10 00:49:08 +0700

    Deploy: 2026-09-10 00:49:08

---
+++ chunk-De30Nqjw.js
@@ -0,0 +1,1 @@
+import{zn as nI}from"./chunk-DbrZ9FvK.js";var i=class e{transform(n){if(n==null)return`—`;let r=Math.round(n);return(r<0?`−`:``)+Math.abs(r).toLocaleString(`vi-VN`)+` ₫`}static ɵfac=function(r){return new(r||e)};static ɵpipe=nI({name:`money`,type:e,pure:!0})};export{i as t};
